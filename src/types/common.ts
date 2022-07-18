0398f932ed4ba3a1b855d8173a953a5753ef98a5
Pitsanu L <pitsanu@deeple.ai>
2022-07-18 23:49:59 +0700

    feat(types): add types common and type for task

---
+++ src/types/common.ts
@@ -0,0 +1,6 @@
+export enum Direction {
+  ASC = 'ASC',
+  DESC = 'DESC',
+}
+
+export type ID = number
---
+++ src/types/task.ts
@@ -0,0 +1,4 @@
+export enum TaskStatus {
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
